7a045e6afe50c1f46a9ca6c36f79ab6cdf7db275
Derek <dereknguyen123@outlook.com>
2022-08-02 12:39:34 -0400

    Spring boot starter.

---
+++ sl-assessment/src/main/java/com/centroid/slassessment/SlAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.centroid.slassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlAssessmentApplication.class, args);
+	}
+
+}
---
+++ sl-assessment/src/test/java/com/centroid/slassessment/SlAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroid.slassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
